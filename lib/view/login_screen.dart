bdf5fa6facb164cb26d97e2fce26642272c4aa0e
sayed adinan <147618795+sayedadinan@users.noreply.github.com>
2024-07-15 17:48:27 +0530

    Custom sized box added

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:love_ally/utils/color_theme/gradient_widget.dart';
+import 'package:love_ally/utils/constants/mediaquery.dart';
+import 'package:love_ally/utils/constants/paths.dart';
+import 'package:love_ally/utils/constants/sizedbox.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -10,7 +13,16 @@ class LoginScreen extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: Column(
-          children: [],
+          children: [
+            const CustomSizedBoxHeight(0.2),
+            Text(
+              'Welcome Back',
+              style: TextStyle(
+                  fontFamily: CustomFonts.splashMainText,
+                  color: Colors.white,
+                  fontSize: mediaquerywidth(0.1, context)),
+            ),
+          ],
         ),
       ),
     );
