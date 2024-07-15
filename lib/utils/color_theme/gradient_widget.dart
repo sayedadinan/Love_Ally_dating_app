09a7747fa025fdd9aa69a13d9092688503d22b28
sayed adinan <147618795+sayedadinan@users.noreply.github.com>
2024-07-15 17:12:14 +0530

    added background color

---
+++ lib/utils/color_theme/gradient_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+// use this widget for the background color of the application
+class GradientBackground extends StatelessWidget {
+  final Widget child;
+
+  const GradientBackground({required this.child, Key? key}) : super(key: key);
+// set the background color of the application
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          stops: [
+            0.0,
+            0.0,
+            1.0,
+          ],
+          colors: [
+            Color.fromARGB(54, 119, 7, 97),
+            Color.fromARGB(255, 37, 1, 27),
+            Colors.black,
+          ],
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+        ),
+      ),
+      child: child,
+    );
+  }
+}
---
+++ lib/view/login_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:love_ally/utils/color_theme/gradient_widget.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GradientBackground(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
